405f6f7f399b40f4514e66e45eb07f29b6bcc21c
joel <1666464510@qq.com>
2021-03-16 12:34:49 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <div>网址输错了哟，检查一下吧</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
