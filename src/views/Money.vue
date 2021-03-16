05ab74056e07bb7f053020217ae4f516b37d2e5c
joel <1666464510@qq.com>
2021-03-16 12:56:28 +0800

    页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>money
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+      <p>money</p>
+
+    </div>
     <Nav/>
   </div>
 </template>
@@ -11,5 +50,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
