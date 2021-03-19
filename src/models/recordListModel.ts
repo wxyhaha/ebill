026d786dffd6086eccebc3aa4b999b7acd623ea0
joel <1666464510@qq.com>
2021-03-20 00:09:03 +0800

    展示和创建标签

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,14 +1,14 @@
 const localStorageKeyName = 'recordList';
-const model = {
+const recordListModel = {
   fetch() {
     return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]')as RecordItem[];
   },
   save(data: RecordItem[]) {
-    window.localStorage.setItem('localStorageKeyName', JSON.stringify(data));
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
   },
   clone(data: RecordItem[] | RecordItem){
     return JSON.parse(JSON.stringify(data))
   }
 };
 
-export default model;
+export default recordListModel;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,17 +14,18 @@ import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
-import model from '@/model';
+import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
 
-const recordList=model.fetch()
-
+const recordList=recordListModel.fetch()
+const tagList=tagListModel.fetch()
 
 
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
-  tags = ['衣', '食', '住', '行'];
+  tags = tagList;
   recordList: RecordItem[] = recordList
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
@@ -43,14 +44,14 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2: RecordItem = model.clone(this.record);
+    const record2: RecordItem = recordListModel.clone(this.record);
     record2.createAt = new Date();
     this.recordList.push(record2);
   }
 
   @Watch('recordList')
   onRecordListChange() {
-    model.save(this.recordList);
+    recordListModel.save(this.recordList);
   }
 }
 </script>
