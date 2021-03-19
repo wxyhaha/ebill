2fc25876c7c4f4d55d1baffcc809c6f19ebaf988
joel <1666464510@qq.com>
2021-03-19 16:55:07 +0800

    完成labels.vue的ts和css

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,26 @@
 <template>
   <Layout>
-    labels
+    <ol class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag"><Icon name="add"/></button>
+    </div>
   </Layout>
 </template>
 
@@ -10,3 +30,36 @@ export default {
 };
 </script>
 
+<style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    svg{
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag{
+  border: none;
+  &-wrapper{
+    text-align: center;
+    padding: 16px ;
+    margin-top: 44-16px;
+  }
+  svg{
+    width: 48px;
+    height: 48px;
+    color: #999999;
+  }
+}
+</style>
