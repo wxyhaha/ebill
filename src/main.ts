eec3a6c96e838ad6905c086e04da5949777ce50c
joel <1666464510@qq.com>
2021-03-21 16:58:02 +0800

    封装record的操作

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
 import tagListModel from '@/models/tagListModel';
+import recordListModel from '@/models/recordListModel';
 
 Vue.config.productionTip = false;
 
@@ -14,6 +15,7 @@ Vue.component('Nav', Nav);
 Vue.component('Layout', Layout);
 Vue.component('Icon', Icon);
 
+//tag store
 window.tagList = tagListModel.fetch();
 window.findTag=(id: string)=>{
   return window.tagList.filter(t => t.id === id)[0];
@@ -32,6 +34,11 @@ window.removeTag = (id: string) => {
 window.updateTag = (id: string, name: string) => {
   return tagListModel.update(id, name);
 };
+
+//record store
+window.recordList=recordListModel.fetch();
+window.createRecord=(record: RecordItem)=> recordListModel.create(record);
+
 
 new Vue({
   router,
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,10 +15,8 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
+import {Component} from 'vue-property-decorator';
 
-const recordList = recordListModel.fetch();
 
 
 @Component({
@@ -26,7 +24,7 @@ const recordList = recordListModel.fetch();
 })
 export default class Money extends Vue {
   tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  recordList = window.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -44,13 +42,10 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record);
+    window.create(this.record);
   }
 
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
-  }
+
 }
 </script>
 
