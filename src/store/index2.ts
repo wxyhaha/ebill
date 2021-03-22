f6d2ff0eeae78eccd4587b96bfa072f98ff74997
joel <1666464510@qq.com>
2021-03-22 14:24:37 +0800

    模块化的store，删除了models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,29 +1,10 @@
-import tagListModel from '@/models/tagListModel';
-import recordListModel from '@/models/recordListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  tagList: tagListModel.fetch(),
-  findTag(id: string) {
-    return this.tagList.filter(t => t.id === id)[0];
-  },
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('此标签已存在');
-    } else if (message === 'success') {
-      window.alert('添加成功');
-    }
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
-
-  //record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record)
+  ...recordStore,
+  ...tagStore
 };
+console.log(store);
 
 export default store;
