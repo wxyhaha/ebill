8542ec1ed22e67b558750bd02c118e4f11ee08c4
joel <1666464510@qq.com>
2021-03-23 11:02:49 +0800

    Tags.vue使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -16,15 +16,18 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 @Component({
-  computed:{
-    tagList(){
-      // return this.$store.fetchTags();
-      return []
+  computed: {
+    tagList() {
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -39,7 +42,7 @@ export default class Tags extends Vue {
   create() {
     const name = window.prompt('请输入新的标签名');
     if (!name) {return window.alert('标签名不能为空');}
-    // store.createTag(name);
+    this.$store.commit('createTag', name);
   }
 }
 </script>
