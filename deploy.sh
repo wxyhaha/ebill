f9cfc722a36333325b5110b63bb02e875694aeaf
joel <1666464510@qq.com>
2021-03-26 17:35:42 +0800

    优化pc界面

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,6 @@ git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:wxyhaha/ebill-website.git master:gh-pages
+git push -f git@gitee.com:joelW/ebill-website.git master:gh-pages
 
 cd -
--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,9 @@ body {
   font-size: 16px;
   background: #f5f5f5;
 }
+
+#app{
+  max-width: 500px;
+  margin: 0 auto;
+}
 </style>
