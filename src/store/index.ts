691a308be546dd182f16b55a2d9e609ab19877b5
joel <1666464510@qq.com>
2021-04-09 13:30:45 +0800

    删除冗余文件

--- src/store/index.ts
+++ src/store/index.ts
@@ -23,7 +23,7 @@ const store = new Vuex.Store({
     },
     createRecord(state, record: RecordItem) {
       const record2 = clone(record);
-      record2.createAt = new Date().toISOString();
+      record2.createAt =record2.createAt || new Date().toISOString();
       state.recordList.push(record2);
       store.commit('saveRecords');
       window.alert('已保存');
