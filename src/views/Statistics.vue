0a693e726fb56b3e56d34f3bcb8cc4ee8a31fa93
joel <1666464510@qq.com>
2021-04-10 12:43:27 +0800

    重构x、y

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,7 +2,7 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <div class="chart-wrapper" ref="chartWrapper">
-      <Chart class="chart" :options="x"/>
+      <Chart class="chart" :options="chartOptions"/>
     </div>
     <ol v-if="groupedList.length>0">
       <li v-for="(group,index) in groupedList" :key="index">
@@ -61,18 +61,18 @@ export default class Statistics extends Vue {
     }
   }
 
-  get y(){
+  get keyValueList(){
     const today = new Date();
     const array = [];
     for (let i = 0; i <= 29; i++) {
       const dateString = dayjs(today).subtract(i, 'day').format('YYYY-MM-DD');
       const found=_.find(this.recordList, {createAt: dateString})
-      array.push({date: dateString, value: found ? found.amount : 0});
+      array.push({key: dateString, value: found ? found.amount : 0});
     }
     array.sort((a,b)=>{
-      if(a.date>b.date){
+      if(a.key>b.key){
         return 1
-      }else if(a.date===b.date){
+      }else if(a.key===b.key){
         return 0;
       }else{
         return -1
@@ -81,10 +81,9 @@ export default class Statistics extends Vue {
     return array;
   }
 
-  get x() {
-
-    const keys=this.y.map(item=>item.date)
-    const values=this.y.map(item=>item.value)
+  get chartOptions() {
+    const keys=this.keyValueList.map(item=>item.key)
+    const values=this.keyValueList.map(item=>item.value)
     return {
       grid: {
         left: 0,
